d35a188e92eb3b934fa9d6398b846803ee1a9853
lfqcamargo <lfqcamargo@gmail.com>
2024-09-20 20:05:15 -0300

    API Adicionando CORS

--- api/src/infra/main.ts
+++ api/src/infra/main.ts
@@ -12,6 +12,30 @@ async function bootstrap() {
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
 
+  const allowedOrigins = [
+    'http://localhost:3000', // Adicione aqui suas origens de front-end
+    'http://localhost:5173',
+    // Adicione outras origens conforme necessário
+  ]
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      console.log(`Origem da requisição: ${origin}`)
+
+      if (!origin) return callback(null, true)
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        console.error(`Origem não permitida: ${origin}`)
+        callback(new Error('Origem não permitida pelo CORS'))
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  })
+
   const logger = new Logger('Bootstrap')
   logger.log(`Application is starting on port ${port}`)
 
